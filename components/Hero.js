ec76645d05c58c7a4ec563759459b55aa67c67c8
Usuario <usuario@MacBook-Pro-de-Heitor.local>
2022-09-27 21:48:57 -0300

    fixed

--- components/Hero.js
+++ components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
 
          <div className='flex max-w-[1280px] justify-between w-full items-center px-8 md:px-4 lg:px-0  my-8'>
         <div className='flex flex-col gap-4'>
-            <h1 className=' text-4xl md:text-5xl lg:text-7xl font-semibold leading-tight w-fit'>Find your new style with <span className='text-[#3865F3]'>Veshion</span></h1>
+            <h1 className=' text-6xl md:text-5xl lg:text-7xl font-semibold leading-tight w-fit'>Find your new style with <span className='text-[#3865F3]'>Veshion</span></h1>
             <p className=' text-[#94959B] text-[20px] w-fit'>Buy something has never been easy now, download Veshion now and get 30 off</p>
             <div className='flex flex-row gap-4 mt-8'>
             <Image
